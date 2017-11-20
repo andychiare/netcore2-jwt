1025b862fbabcef96c3239918859a043c10df3af
Andrea <andrea.chiarelli@apparound.com>
2017-11-20 18:35:30 +0100

    Added integration tests

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 This is a .NET Core 2 sample project showing how to use JWTs (JSON Web Tokens).
 
 
+##Installation
+In order to run the project, you need Visual Studio 2017.
+In order to test the application, you need Postman or another HTTP client.
+
+##Test
+1. Run the project from Visual Studio 2017 and take note of the URL opened in your browser (e.g. _http://localhost:63939_).
+
 
---
+++ Test/ApplicationTest.cs
@@ -0,0 +1,77 @@
+using JWT;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class ApplicationTest
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        public ApplicationTest()
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.GetFullPath(@"../../.."))
+            .AddJsonFile("appsettings.json", optional: false)
+            //.AddUserSecrets<Startup>()
+            .Build();
+
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseConfiguration(configuration));
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task UnAuthorizedAccess()
+        {
+            var response = await _client.GetAsync("/api/books");
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetToken()
+        {
+            var bodyString = @"{username: ""mario"", password: ""secret""}";
+            var response = await _client.PostAsync("/api/token", new StringContent(bodyString, Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseJson = JObject.Parse(responseString);
+            Assert.NotNull((string)responseJson["token"]);
+        }
+
+        [Fact]
+        public async Task GetBooks()
+        {
+            var bodyString = @"{username: ""mario"", password: ""secret""}";
+            var response = await _client.PostAsync("/api/token", new StringContent(bodyString, Encoding.UTF8, "application/json"));
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseJson = JObject.Parse(responseString);
+            var token = (string)responseJson["token"];
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "/api/books");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var booksResponse = await _client.SendAsync(requestMessage);
+
+            Assert.Equal(HttpStatusCode.OK, booksResponse.StatusCode);
+
+            var bookResponseString = await booksResponse.Content.ReadAsStringAsync();
+            var bookResponseJson = JArray.Parse(bookResponseString);
+            Assert.Equal(true, bookResponseJson.Count > 0);
+        }
+    }
+}
