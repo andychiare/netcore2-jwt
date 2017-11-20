a32dce7d8ba6a326b5cd561648fb752de89bac40
Andrea <andrea.chiarelli@apparound.com>
2017-11-20 12:05:40 +0100

    Initial commit

---
+++ JWT1/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet, Authorize]
+        public IEnumerable<Book> Get()
+        {
+            return new Book[] {
+                new Book { Author = "Ray Bradbury", Title = "Fahrenheit 451" },
+                new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude" },
+                new Book { Author = "George Orwell", Title = "1984" } };
+        }
+
+        public class Book
+        {
+            public string Author { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
---
+++ JWT1/Controllers/TokenController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = Authenticate(login);
+
+            if (user != null)
+            {
+                var tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(UserModel user)
+        {
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Name),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+               };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(LoginModel login)
+        {
+            UserModel user = null;
+
+            if (login.Username == "mario" && login.Password == "secret")
+            {
+                user = new UserModel { Name = "Mario Rossi", Email = "mario.rossi@domain.com" };
+            }
+
+            return user;
+
+        }
+
+        public class LoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        private class UserModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
